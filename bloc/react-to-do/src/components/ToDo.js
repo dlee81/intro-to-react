86f3cb868da7b15b36ea7ae1768bd35f48fdfa0d
David L <davidjlee@gmail.com>
2019-01-11 10:40:28 -0500

    checkpoint-7-reacttodostateandprops:  completed

--- bloc/react-to-do/src/components/ToDo.js
+++ bloc/react-to-do/src/components/ToDo.js
@@ -3,9 +3,14 @@ import React, { Component } from 'react';
  class ToDo extends Component {
    render() {
      return (
-       <li> A todo will go here </li>
+       //<li> A todo will go here </li>
+       <li>{ this.props.description }</li>
+        <li>
+         <input type="checkbox" checked={ this.props.isCompleted } />
+         <span>{ this.props.description }</span>
+       </li>
      );
    }
  }
-  
+
  export default ToDo;
